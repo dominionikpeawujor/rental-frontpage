4146e43c8de2cf2d9b7e55f0c358328309a3629a
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-04-28 10:09:27 +0100

    Feat & Style: Add responsiveness to wider screen
    I added the responsive features of the website for screens wider than
    2048px and 3840px.
    
    I also added the scroll reveal feature.

--- assets/js/main.js
+++ assets/js/main.js
@@ -130,3 +130,10 @@ themeButton.addEventListener('click', () => {
 
 
 // =================== SCROLL REVEAL ANIMATION ===================
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: .3
+});
+

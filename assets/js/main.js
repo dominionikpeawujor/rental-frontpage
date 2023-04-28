8432ea2e9f8bd8b94ae6c877ad5231ff87cba9e1
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-04-28 22:43:09 +0100

    Style: Add Transitions to text and components
    I used a package called scrollreveal to add some animations as I scroll
    to sections of the page. It makes the site more interesting.

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,6 +134,16 @@ const sr = ScrollReveal({
   origin: 'top',
   distance: '60px',
   duration: 2500,
-  delay: .3
+  delay: 400,
 });
+
+sr.reveal('.home__title, .popular__container, .subscribe__container, .footer__container');
+sr.reveal('.home__description, .footer__info ', {delay: 500});
+sr.reveal('.home__search', {delay: 600});
+sr.reveal('.home__value', {delay: 700});
+sr.reveal('.home__images', {delay: 800, bottom});
+sr.reveal('.logos__img', {interval: 100});
+sr.reveal(`.value__images, .contact__content`, {origin: 'left'});
+sr.reveal('.value__content, .contact__images', {origin: 'right'});
+
 
